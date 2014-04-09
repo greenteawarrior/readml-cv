5ccc75bd43a9520c1a60562649b512fce95777fa
greenteawarrior <emily.wang@students.olin.edu>
2014-04-09 19:30:50 -0400

    attempt at compareHist

--- ExtractWaffles.py
+++ ExtractWaffles.py
@@ -26,13 +26,19 @@ def display_image_and_wait(image):
     
 def print_rgb_hist(img, mask):
     color = ('b','g','r')
-    print mask
     for i,col in enumerate(color):
         histr = cv2.calcHist([img],[i],mask,[256],[0,256])
         plt.plot(histr,color = col)
         plt.xlim([0,256])
     plt.show()
     
+def compare_hist(img1,mask1,img2,mask2):
+    color = ('b','g','r')
+    for i,col in enumerate(color):
+        h1 = cv2.calcHist([img1],[i],mask1,[256],[0,256])
+        h2 = cv2.calcHist([img2],[i],mask2,[256],[0,256])
+    print cv2.compareHist(h1,h2,CV_COMP_CORREL)
+
 def crop_waffle(img):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     greyscale = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -56,12 +62,11 @@ def crop_waffle(img):
     shape = img.shape
     largest_blob_mask = np.zeros((shape[0],shape[1],1),np.uint8)
     cv2.fillPoly(largest_blob_mask, pts =[max_contour], color=(255,255,255))
-    print largest_blob_mask
-    print_rgb_hist(img,largest_blob_mask)
+    # print_rgb_hist(img,largest_blob_mask)
     return cv2.bitwise_and(img,img, mask= largest_blob_mask)
 
 def main():
-    waffle_folder_name = os.getcwd() + '/waffles_images'
+    waffle_folder_name = os.getcwd() + '/waffle_images'
     new_img_dir = os.getcwd() + '/modified_waffle_images'
     
     if not os.path.isdir(waffle_folder_name):
@@ -71,11 +76,26 @@ def main():
         
     for index, pic in enumerate(os.listdir(waffle_folder_name)):
         loaded_pic = cv2.imread(waffle_folder_name + '/' + pic) 
+        print waffle_folder_name + '/' + pic
+        print type(loaded_pic)
         print 'reading: ' + waffle_folder_name + '/' + pic
         new_img_name = 'waffle_pic_'+ str(index) + '.jpg'
         cropped_pic = crop_waffle(loaded_pic)
         display_image_and_wait(cropped_pic)
         cv2.imwrite(new_img_dir + '/' + new_img_name,cropped_pic)
 
-main()
+def compare_hist_tester():
+
+    waffle_folder_name = os.getcwd() + '/waffle_images'
+    new_img_dir = os.getcwd() + '/modified_waffle_images'    
+    
+    img1 = cv2.imread(waffle_folder_name+"/w45.jpeg")
+    img2 = cv2.imread(waffle_folder_name+"/w5.jpeg")
+    mask1 = crop_waffle(img1)
+    mask2 = crop_waffle(img2)
+    print mask1.dtype
+    print mask2.dtype
+    compare_hist(img1,mask1,img2,mask2)
+
+compare_hist_tester()
 
