f9d31477603104a7ac3aefc20c0d91755f83d4e4
greenteawarrior <emily.wang@students.olin.edu>
2014-04-15 23:02:46 -0400

    histograms in progress

--- ExtractWaffles.py
+++ ExtractWaffles.py
@@ -2,17 +2,18 @@
 Created on Apr 1, 2014
 
 @author: dchen
+more authors: jenwei and greenteawarrior
 
 extract waffles from the flickr imageset that jwei downloaded
 using hue thresholding followed by histogram backprojection 
 
-after segmenting the waffles and normalizing them to create a dataset, we'll take another approach by 
-running keypoint identification followed by kmeans clustering
-followed by logsitic regression to identify something as waffle 
-or non waffle
+after segmenting the waffles and normalizing them to create a dataset, 
+we'll take another approach by running keypoint identification 
+followed by kmeans clustering followed by logsitic regression 
+to identify something as waffle or non waffle
 
-^the second paragraph is pointless but its a ML exercise
 '''
+
 import cv2.cv as cv
 import cv2
 import os
@@ -23,23 +24,15 @@ def display_image_and_wait(image):
     cv2.imshow('dst',image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    
-def print_rgb_hist(img, mask):
-    color = ('b','g','r')
-    for i,col in enumerate(color):
-        histr = cv2.calcHist([img],[i],mask,[256],[0,256])
-        plt.plot(histr,color = col)
-        plt.xlim([0,256])
-    plt.show()
-    
+        
 def compare_hist(img1,mask1,img2,mask2):
     color = ('b','g','r')
     for i,col in enumerate(color):
-        h1 = cv2.calcHist([img1],[i],mask1,[256],[0,256])
-        h2 = cv2.calcHist([img2],[i],mask2,[256],[0,256])
+        h1 = cv2.calcHist([img1],[i],mask1,[8],[0,256])
+        h2 = cv2.calcHist([img2],[i],mask2,[8],[0,256])
     print cv2.compareHist(h1,h2,CV_COMP_CORREL)
 
-def crop_waffle(img):
+def crop_mask_waffle(img):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     greyscale = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     lower_yellow = np.array([0,50,50])
@@ -63,9 +56,9 @@ def crop_waffle(img):
     largest_blob_mask = np.zeros((shape[0],shape[1],1),np.uint8)
     cv2.fillPoly(largest_blob_mask, pts =[max_contour], color=(255,255,255))
     # print_rgb_hist(img,largest_blob_mask)
-    return cv2.bitwise_and(img,img, mask= largest_blob_mask)
+    return [cv2.bitwise_and(img,img, mask= largest_blob_mask), largest_blob_mask]
 
-def main():
+def waffle_directories():
     waffle_folder_name = os.getcwd() + '/waffle_images'
     new_img_dir = os.getcwd() + '/modified_waffle_images'
     
@@ -73,7 +66,13 @@ def main():
         os.makedirs(waffle_folder_name)
     if not os.path.isdir(new_img_dir):
         os.makedirs(new_img_dir)
-        
+
+    return waffle_folder_name, new_img_dir
+
+def crop_waffle_tester(paths):    
+    waffle_folder_name = paths[0]
+    new_img_dir = paths[1]
+
     for index, pic in enumerate(os.listdir(waffle_folder_name)):
         loaded_pic = cv2.imread(waffle_folder_name + '/' + pic) 
         print waffle_folder_name + '/' + pic
@@ -84,18 +83,43 @@ def main():
         display_image_and_wait(cropped_pic)
         cv2.imwrite(new_img_dir + '/' + new_img_name,cropped_pic)
 
-def compare_hist_tester():
+def print_rgb_hist(img, mask):
+    color = ('b','g','r')
+    for i,col in enumerate(color):
+        histr = cv2.calcHist([img],[i],mask.astype(np.uint8),[8],[0,256])
+        plt.plot(histr,color = col)
+        plt.xlim([0,256])
+    plt.show()
 
-    waffle_folder_name = os.getcwd() + '/waffle_images'
-    new_img_dir = os.getcwd() + '/modified_waffle_images'    
+def makeHistograms(paths):
     
-    img1 = cv2.imread(waffle_folder_name+"/w45.jpeg")
-    img2 = cv2.imread(waffle_folder_name+"/w5.jpeg")
-    mask1 = crop_waffle(img1)
-    mask2 = crop_waffle(img2)
-    print mask1.dtype
-    print mask2.dtype
-    compare_hist(img1,mask1,img2,mask2)
+    waffle_folder_name = paths[0]
+    new_img_dir = paths[1]
 
-compare_hist_tester()
+    img = cv2.imread(waffle_folder_name+"/w4.jpeg")
+    shape = img.shape
+
+    # hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    # greyscale = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    # lower_yellow = np.array([0,50,50])
+    # upper_yellow = np.array([70,255,255])
+    # mask = cv2.inRange(hsv, np.uint8(lower_yellow), np.uint8(upper_yellow))
+    # masked_img = cv2.bitwise_and(img,img, mask= mask)
+
+    mask = crop_mask_waffle(img)[1]
+    color = ('b','g','r')
+    res = []
+    for i,col in enumerate(color):
+        h = cv2.calcHist([img],[i],mask,[8],[0,256])
+        h = [num[0] for num in h]
+        res.append(h)
+    
+    display_image_and_wait(img)
+    print res
+
+    return
+    
+if __name__ == "__main__":
+    paths = waffle_directories()
+    makeHistograms(paths)
 
