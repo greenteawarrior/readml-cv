dcc86222dd934ba1c6a4bf64ab19f2f80e835dc7
greenteawarrior <emily.wang@students.olin.edu>
2014-04-15 23:11:38 -0400

    cleaning up a bit

--- ExtractWaffles.py
+++ ExtractWaffles.py
@@ -32,6 +32,39 @@ def compare_hist(img1,mask1,img2,mask2):
         h2 = cv2.calcHist([img2],[i],mask2,[8],[0,256])
     print cv2.compareHist(h1,h2,CV_COMP_CORREL)
 
+def crop_waffle_tester(paths):    
+    waffle_folder_name = paths[0]
+    new_img_dir = paths[1]
+
+    for index, pic in enumerate(os.listdir(waffle_folder_name)):
+        loaded_pic = cv2.imread(waffle_folder_name + '/' + pic) 
+        print waffle_folder_name + '/' + pic
+        print type(loaded_pic)
+        print 'reading: ' + waffle_folder_name + '/' + pic
+        new_img_name = 'waffle_pic_'+ str(index) + '.jpg'
+        cropped_pic = crop_waffle(loaded_pic)
+        display_image_and_wait(cropped_pic)
+        cv2.imwrite(new_img_dir + '/' + new_img_name,cropped_pic)
+
+def print_rgb_hist(img, mask):
+    color = ('b','g','r')
+    for i,col in enumerate(color):
+        histr = cv2.calcHist([img],[i],mask.astype(np.uint8),[8],[0,256])
+        plt.plot(histr,color = col)
+        plt.xlim([0,256])
+    plt.show()
+
+def waffle_directories():
+    waffle_folder_name = os.getcwd() + '/waffle_images'
+    new_img_dir = os.getcwd() + '/modified_waffle_images'
+    
+    if not os.path.isdir(waffle_folder_name):
+        os.makedirs(waffle_folder_name)
+    if not os.path.isdir(new_img_dir):
+        os.makedirs(new_img_dir)
+
+    return waffle_folder_name, new_img_dir
+
 def crop_mask_waffle(img):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     greyscale = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -58,39 +91,6 @@ def crop_mask_waffle(img):
     # print_rgb_hist(img,largest_blob_mask)
     return [cv2.bitwise_and(img,img, mask= largest_blob_mask), largest_blob_mask]
 
-def waffle_directories():
-    waffle_folder_name = os.getcwd() + '/waffle_images'
-    new_img_dir = os.getcwd() + '/modified_waffle_images'
-    
-    if not os.path.isdir(waffle_folder_name):
-        os.makedirs(waffle_folder_name)
-    if not os.path.isdir(new_img_dir):
-        os.makedirs(new_img_dir)
-
-    return waffle_folder_name, new_img_dir
-
-def crop_waffle_tester(paths):    
-    waffle_folder_name = paths[0]
-    new_img_dir = paths[1]
-
-    for index, pic in enumerate(os.listdir(waffle_folder_name)):
-        loaded_pic = cv2.imread(waffle_folder_name + '/' + pic) 
-        print waffle_folder_name + '/' + pic
-        print type(loaded_pic)
-        print 'reading: ' + waffle_folder_name + '/' + pic
-        new_img_name = 'waffle_pic_'+ str(index) + '.jpg'
-        cropped_pic = crop_waffle(loaded_pic)
-        display_image_and_wait(cropped_pic)
-        cv2.imwrite(new_img_dir + '/' + new_img_name,cropped_pic)
-
-def print_rgb_hist(img, mask):
-    color = ('b','g','r')
-    for i,col in enumerate(color):
-        histr = cv2.calcHist([img],[i],mask.astype(np.uint8),[8],[0,256])
-        plt.plot(histr,color = col)
-        plt.xlim([0,256])
-    plt.show()
-
 def makeHistograms(paths):
     
     waffle_folder_name = paths[0]
@@ -99,24 +99,15 @@ def makeHistograms(paths):
     img = cv2.imread(waffle_folder_name+"/w4.jpeg")
     shape = img.shape
 
-    # hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    # greyscale = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    # lower_yellow = np.array([0,50,50])
-    # upper_yellow = np.array([70,255,255])
-    # mask = cv2.inRange(hsv, np.uint8(lower_yellow), np.uint8(upper_yellow))
-    # masked_img = cv2.bitwise_and(img,img, mask= mask)
-
     mask = crop_mask_waffle(img)[1]
     color = ('b','g','r')
     res = []
     for i,col in enumerate(color):
-        h = cv2.calcHist([img],[i],mask,[8],[0,256])
-        h = [num[0] for num in h]
-        res.append(h)
+        channel = cv2.calcHist([img],[i],mask,[8],[0,256])
+        channel = [num[0] for num in channel]
+        res.append(channel)
     
-    display_image_and_wait(img)
     print res
-
     return
     
 if __name__ == "__main__":
